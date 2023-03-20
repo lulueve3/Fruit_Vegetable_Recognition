965098ef34bbfdc9f9d2aa150ea4e34936150a28
Trần Ngọc Hải <89736351+lulueve3@users.noreply.github.com>
2023-03-20 14:01:19 +0700

    Update Fruits_Vegetable_Classification.py

--- Fruits_Vegetable_Classification.py
+++ Fruits_Vegetable_Classification.py
@@ -5,6 +5,7 @@ import numpy as np
 from keras.models import load_model
 import requests
 from bs4 import BeautifulSoup
+import os
 
 model = load_model('FV.h5')
 labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
@@ -55,6 +56,8 @@ def run():
         img = Image.open(img_file).resize((250, 250))
         st.image(img, use_column_width=False)
         save_image_path = './upload_images/' + img_file.name
+        if not os.path.exists('./upload_images'):
+            os.makedirs('./upload_images')
         with open(save_image_path, "wb") as f:
             f.write(img_file.getbuffer())
 
